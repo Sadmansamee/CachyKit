01fff33ed39aa7f9564dfe95613855224800e556
Sadman Samee <sadman.tonmoy@gmail.com>
2019-09-07 12:04:40 +0600

    Project reorientation
    1. Project restructure
    2. Ready for pod

---
+++ Example/CachyDemo/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  Cachy
+//
+//  Created by sadman samee on 9/4/19.
+//  Copyright © 2019 sadman samee. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+    func applicationWillResignActive(_: UIApplication) {
+      
+    }
+
+    func applicationDidEnterBackground(_: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(_: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(_: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(_: UIApplication) {
+      
+    }
+}
